6c359bdc854fb55a9ddb2e745a7ae627f189268f
Thomas Foster <thomas.foster80@gmail.com>
2014-01-12 07:52:01 -0500

    modified functions

--- simple.rb
+++ simple.rb
@@ -1,8 +1,12 @@
 !#/usr/bin/env ruby
 #'1' * 8 + '.' + '1' * 8 + '.' + '1' * 7 + '0' + '.' + '0' * 8
 
-
 def convert_to_bin(num)
+    
+    if (num < 8) || (num > 32)
+        puts "Invalid subnet range #{num}."
+        exit
+    end
 
     groupofbits = num / 8
     numberofones = groupofbits * 8 + (num % 8)
@@ -17,32 +21,29 @@ def convert_to_bin(num)
 end
 
 
-def find_my_networks(network)
+def networks_hosts(network)
     
     define_network = network.rindex('1')
+    define_hosts = network.scan('0').length
     mynet = (define_network + 1) % 8
+    myhosts = (2 ** define_hosts) - 2
     
     mynetworks =  mynet == 0 ? 8 : mynet 
     puts mynetworks
+    puts define_hosts
     output_networks = 2 ** (8 - mynetworks)
     available_networks = output_networks == 1 ? 1 : (256 / output_networks) 
     
+
     puts output_networks
-    puts "You have #{available_networks} networks available"
+    puts "You have #{available_networks} networks available with a total of #{myhosts} valid ip's"
     
     #Add check for if available_networks is 1 and if so this is a specific ip address
 
-    for i in (0..256)
-        print i % output_networks == 0 ? "Mynetworks #{i} \n" : nil
-    end
+    #for i in (0..256)
+    #    print i % output_networks == 0 ? "Mynetworks #{i} \n" : nil
+    #end
    
-end
-
-
-def find_my_hosts(hosts)
-
-
-
 end
 
 
@@ -50,4 +51,4 @@ print "Subnet: "
 mysubnet = gets.chomp
 
 
-find_my_networks(convert_to_bin(mysubnet.to_i))
+networks_hosts(convert_to_bin(mysubnet.to_i))
