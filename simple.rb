82ea798aad4a37282f439a66463f7931ef24c90c
Thomas Foster <thomas.foster80@gmail.com>
2014-01-13 04:31:50 -0500

    using objects

--- simple.rb
+++ simple.rb
@@ -1,57 +1,62 @@
 !#/usr/bin/env ruby
-#'1' * 8 + '.' + '1' * 8 + '.' + '1' * 7 + '0' + '.' + '0' * 8
 
-def convert_to_bin(num)
-    
-    if (num < 8) || (num > 32)
-        puts "Invalid subnet range #{num}."
+class Network
+
+end
+
+class IP < Network
+
+  def initialize(myip, mymask) 
+      @myip   = myip
+      @mymask = mymask
+  end
+  
+  def convert_to_bin 
+      
+     if (@mymask < 8) || (@mymask > 32)
+        puts "Invalid subnet range #{@mymask}."
         exit
     end
 
-    groupofbits = num / 8
-    numberofones = groupofbits * 8 + (num % 8)
+    groupofbits = @mymask / 8
+    numberofones = groupofbits * 8 + (@mymask % 8)
     zerosleft = 32 - numberofones
     
     #print "Groups of 8 bits #{groupofbits}, How many number of ones #{numberofones}, My zero's left #{zerosleft}"
     #puts
+            
+    @mybinary = '1' * numberofones + '0' * zerosleft
     
-    mybinary = '1' * numberofones + '0' * zerosleft
-    return mybinary
+  end
+  
+  def networks_hosts
     
+      define_network = @mybinary.rindex('1')
+      define_hosts = @mybinary.scan('0').length
+      mynet = (define_network + 1) % 8
+      myhosts = (2 ** define_hosts) - 2 < 1 ? 1 : (2 ** define_hosts) - 2
+  
+      mynetworks =  mynet == 0 ? 8 : mynet
+      #puts mynetworks
+      #puts define_hosts
+      output_networks = 2 ** (8 - mynetworks)
+      available_networks = output_networks == 1 ? 1 : (256 / output_networks)
+  
+  
+      #puts output_networks
+      print "You have #{available_networks} networks available with a total of #{myhosts} valid ip's"
+  
+      #Add check for if available_networks is 1 and if so this is a specific ip address
+  
+      #for i in (0..256)
+      #    print i % output_networks == 0 ? "Mynetworks #{i} \n" : nil
+      #end
+
+   end
+
 end
 
 
-def networks_hosts(network)
-    
-    define_network = network.rindex('1')
-    define_hosts = network.scan('0').length
-    mynet = (define_network + 1) % 8
-    myhosts = (2 ** define_hosts) - 2 < 1 ? 1 : (2 ** define_hosts) - 2
-    
-    mynetworks =  mynet == 0 ? 8 : mynet 
-    #puts mynetworks
-    #puts define_hosts
-    output_networks = 2 ** (8 - mynetworks)
-    available_networks = output_networks == 1 ? 1 : (256 / output_networks) 
-    
-
-    #puts output_networks
-    puts "You have #{available_networks} networks available with a total of #{myhosts} valid ip's"
-    
-    #Add check for if available_networks is 1 and if so this is a specific ip address
-
-    #for i in (0..256)
-    #    print i % output_networks == 0 ? "Mynetworks #{i} \n" : nil
-    #end
-   
-end
-
-print "IP address/subnet: "
-mysubnet = gets.chomp
-
-myinput = mysubnet.split('/')
-
-#print myinput[1]
-
-networks_hosts(convert_to_bin(myinput[1].to_i))
-#networks_hosts(convert_to_bin(mysubnet.to_i))
+foster = IP.new('192.168.1.1', 24)
+#puts foster.convert_to_bin
+#puts foster.networks_hosts
