06ed791a22fdc481a3bd436d43be1c52f2a7f4e7
Thomas Foster <tfoster@promnetwork.com>
2014-02-07 10:13:44 -0500

    cleanup for better readability

--- simple.rb
+++ simple.rb
@@ -6,79 +6,76 @@ end
 
 class IP < Network
 
-  def subnet_to_bin
-      
-     if (@mymask < 8) || (@mymask > 32)
-        puts "Invalid subnet range #{@mymask}."
-        exit
-    end
-
-    groupofbits = @mymask / 8
-    numberofones = groupofbits * 8 + (@mymask % 8)
-    zerosleft = 32 - numberofones
-    
-    #print "Groups of 8 bits #{groupofbits}, How many number of ones #{numberofones}, My zero's left #{zerosleft}"
-    #puts
-            
-    @mybinary = '1' * numberofones + '0' * zerosleft
-    #return
-    
-  end
-  
-  def networks
-    
-      define_network = @mybinary.rindex('1')
-      mynet = (define_network + 1) % 8
-  
-      mynetworks =  mynet == 0 ? 8 : mynet
-      output_networks = 2 ** (8 - mynetworks)
-      available_networks = output_networks == 1 ? 1 : (256 / output_networks)
-  
-      print "You have #{available_networks} networks available"
-  
-   end
-
-  def hosts
-
-    define_hosts = @mybinary.scan('0').length
-    myhosts = (2 ** define_hosts) - 2 < 1 ? 1 : (2 ** define_hosts) - 2
-    
-    #puts myhosts
-    print "You have #{myhosts} valid ip's"
-  
-  end
-
-  def initialize(myip, network)
+   def initialize(myip, network)
 
            @myip   = myip.split('.')
            @mymask = network.to_i
 
-  end
+   end
+  
 
-  def ip_to_bin
- 
-      convert_ip = @myip
+   def subnet_to_bin
+      
+         if (@mymask < 8) || (@mymask > 32)
+            puts "Invalid subnet range #{@mymask}."
+            exit
+         end
 
-
-     @myconvip = Array.new 
-     convert_ip.each do |mynum|
+        groupofbits = @mymask / 8
+        numberofones = groupofbits * 8 + (@mymask % 8)
+        zerosleft = 32 - numberofones
     
-       @myconvip.push(convert_to_bin(mynum.to_i))
+        @mybinary = '1' * numberofones + '0' * zerosleft
+    
+   end
+  
+   def networks
+    
+        define_network = @mybinary.rindex('1')
+        mynet = (define_network + 1) % 8
+  
+        mynetworks =  mynet == 0 ? 8 : mynet
+        output_networks = 2 ** (8 - mynetworks)
+        available_networks = output_networks == 1 ? 1 : (256 / output_networks)
+  
+        print "You have #{available_networks} networks available"
+  
+   end
+
+   def hosts
+
+        define_hosts = @mybinary.scan('0').length
+        myhosts = (2 ** define_hosts) - 2 < 1 ? 1 : (2 ** define_hosts) - 2
+    
+        print "You have #{myhosts} valid ip's"
+  
+   end
+
+
+   def ip_to_bin
+ 
+       convert_ip = @myip
+
+
+       @myconvip = Array.new 
+       convert_ip.each do |mynum|
+    
+          @myconvip.push(convert_to_bin(mynum.to_i))
         
-     end
+       end
 
-    puts @myconvip.join('.')
+       puts @myconvip.join('.')
 
-  end
+   end
 
   def convert_to_bin(cNUM)
   
-       convert = cNUM.to_s(2)
+        convert = cNUM.to_s(2)
          if convert.length != 8
-            leftbits = 8 - convert.length
-           return newconvert =  '0' * leftbits + convert
+             leftbits = 8 - convert.length
+            return newconvert =  '0' * leftbits + convert
         else
-           return convert
+            return convert
         end
   end
 
